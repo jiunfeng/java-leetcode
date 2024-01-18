cd8b98ad21fd320906ab0e7787eed8ee76ab5fe1
Jiunfeng <marcus@gmail.com>
2024-01-18 09:18:25 +0800

    24-01-18

---
+++ leetcode/src/main/java/com/leetcode/LengthofLastWord58/App.java
@@ -0,0 +1,44 @@
+package com.leetcode.LengthofLastWord58;
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ */
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        String[] words = s.split("\\s+");
+
+        return words[words.length - 1].length();
+
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int solReInt = sol.lengthOfLastWord("   fly me   to   the moon  ");
+        System.out.println(solReInt);
+    }
+}
