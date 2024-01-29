661ad20b9e4af8b1386461b1d1cf248188f183b5
marcustzeng <jiunfengtzeng@gmail.com>
2024-01-29 09:08:42 +0800

    merge test01

--- leetcode/src/main/java/com/leetcode/SearchInsertPosition35/App.java
+++ leetcode/src/main/java/com/leetcode/SearchInsertPosition35/App.java
@@ -17,12 +17,12 @@ Example 3:
 Input: nums = [1,3,5,6], target = 7
 Output: 4
  */
-
+DEE
 class Solution {
     public int searchInsert(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
+            if (nums[i] == targetdd) {
                 return i;
             } else if (nums[i] > target) {
                 return i;
